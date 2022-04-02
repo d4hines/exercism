3eb338f712893b56574b8d44fbe62897a80c599b
Daniel Hines <d4hines@gmail.com>
2022-04-02 19:09:39 -0400

    hamming distance solution

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,5 +1,16 @@
 type nucleotide = A | C | G | T
 
-let hamming_distance _a _b =
-  let _x = "left and right strands must be of equal length" in
-  assert false
+let hamming_distance a b =
+  match (a, b) with
+  | [], [] -> Ok 0
+  | [], _ -> Error "left strand must not be empty"
+  | _, [] -> Error "right strand must not be empty"
+  | a, b when List.length a <> List.length b ->
+      Error "left and right strands must be of equal length"
+  | a, b ->
+      let distance =
+        List.fold_left
+          (fun distance (a, b) -> if a = b then distance else distance + 1)
+          0 (List.combine a b)
+      in
+      Ok distance
