744d8d8d194040dd3c1c4eb950d6c893bcbb4998
Daniel Hines <d4hines@gmail.com>
2022-04-02 20:42:42 -0400

    nucleotide count

---
+++ ocaml/src/nucleotide-count/nucleotide_count.ml
@@ -0,0 +1,18 @@
+open Base
+
+let empty = Map.empty (module Char)
+let is_valid_char = function 'A' | 'C' | 'G' | 'T' -> true | _ -> false
+
+let count_nucleotide s c =
+  if is_valid_char c then
+    String.fold_result s ~init:0 ~f:(fun count -> function
+      | char when Char.equal c char -> Ok (count + 1)
+      | char when is_valid_char char -> Ok count
+      | char -> Error char)
+  else Error c
+
+let count_nucleotides s =
+  String.fold_result s ~init:empty ~f:(fun map c ->
+      if is_valid_char c then
+        Ok (Map.update map c ~f:(function None -> 1 | Some n -> n + 1))
+      else Error c)
