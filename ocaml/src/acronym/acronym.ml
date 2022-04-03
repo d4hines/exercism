1a81215b3c08255aa54c1a4b1a1bd27650194987
Daniel Hines <d4hines@gmail.com>
2022-04-02 21:34:48 -0400

    acronym: less concats

--- ocaml/src/acronym/acronym.ml
+++ ocaml/src/acronym/acronym.ml
@@ -8,10 +8,12 @@ let word_delimiters = [' '; '-'; ',' ]
 let is_word_delimiter = List.mem word_delimiters ~equal:Char.equal
 
 let acronym =
-  String.fold ~init:("", true) ~f:(fun (acc, take_next_alpha_char) c ->
+  String.fold ~init:([], true) ~f:(fun (acc, take_next_alpha_char) c ->
     match c with
     | c when is_word_delimiter c -> acc, true
-    | c when Char.is_alpha c && take_next_alpha_char -> acc ^ (Char.uppercase c |> String.of_char), false
+    | c when Char.is_alpha c && take_next_alpha_char -> (Char.uppercase c) :: acc, false
     | _ -> acc, take_next_alpha_char
     )
     >> fst
+    >> List.rev
+    >> String.of_char_list
